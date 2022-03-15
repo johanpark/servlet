2163223cabf5e9661b08faccadbc34e57f955c97
johanpark <pyhan23@gmail.com>
2022-03-15 16:48:14 +0900

    2022-03-15 commit

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -1,9 +1,32 @@
 package hello.servlet.basic.response;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name="responseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //[status-line]
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setHeader("Content-type", "text/plain");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header","hello");
+
+        //[Header 편의 메서드]
+     /*   content(resp);
+        cookie(resp);
+        redirect(resp);*/
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+
 
 }
